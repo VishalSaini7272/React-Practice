5f4acc556b8938c2adce3965a18a7691ee5e1237
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-27 01:29:12 +0530

    new code added

---
+++ Personal_CV/src/components/About.jsx
@@ -0,0 +1,12 @@
+function About(){
+
+    return(
+        <>
+        <div>
+            <h1>Welcome to my About page</h1>
+            <div><img src="https://images.unsplash.com/photo-1761165308412-21acb4dae3b2?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600" alt="tree" /></div>
+        </div>
+        </>
+    )
+}
+export default About;
